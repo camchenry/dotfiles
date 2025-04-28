5473d164ee02413ffdbf312c68b94e24deea04bc
Cam McHenry <camchenry@users.noreply.github.com>
2025-04-28 11:55:54 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Installing dotfiles..."
+
+# Detect if running on macOS
+if [[ "$(uname)" == "Darwin" ]]; then
+  echo "Running on macOS"
+  ./macos-instant-show-dock
+fi
+
+# Detect if running in a GitHub Codespace
+if [[ -n "$CODESPACES" ]]; then
+  echo "Running in a GitHub Codespace"
+fi
